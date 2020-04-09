7511ed6b50db6a9cfcd86da9b4040f4a879d3a6b
De Xian <ahx.agent007@gmail.com>
2020-04-09 23:56:50 +0600

    Add Rescure design

---
+++ app/src/main/java/com/dexian/robinhood/AddRescue.java
@@ -0,0 +1,14 @@
+package com.dexian.robinhood;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddRescue extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_rescue);
+    }
+}
